00e0c5540d391436d0cb698245b734c7a202d364
nursultan-tech <54637703+nursultan-tech@users.noreply.github.com>
2022-07-16 23:07:38 +0600

    Add files via upload

---
+++ summerSchool/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InstaCard from './components/instaCard/InstaCard';
+
+function App() {
+  return (
+    <div className="App">
+      <InstaCard />
+    </div>
+  );
+}
+
+export default App;
